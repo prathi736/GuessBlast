a6a31ba599d26200086fcee24c9718949db85962
Pulkit Rathi <63576303+prathi736@users.noreply.github.com>
2023-11-20 11:08:05 +0530

    update close button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Modal({ isCorrect, turn, sol}) {
+
+    const[modal, setModal] = useState(true);
+
+    const closeModal = () => {
+        setModal(false);
+        // reloads the current document
+        window.location.reload();
+    };
+
     return (
-        <div className='modal'>
-            {isCorrect && (
-                <div>
-                    <h1>You Won!!</h1>
-                    <p className='solution'>Answer: {sol}</p>
-                    <p>You found the solution in {turn} guesses 😃</p>
-                    {/* <button onClick={() => {  onClose(); }}>Close</button> */}
+        <>
+            {modal && (
+                <div className='modal'>
+                    {isCorrect ? (
+                        <div>
+                            <h1>You Won!!</h1>
+                            <p className='solution'>Answer: {sol}</p>
+                            <p>You found the solution in {turn} guesses 😃</p>
+                            <button onClick={closeModal}>Close</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h1>Never Give Up!!</h1>
+                            <p className='solution'>Answer: {sol}</p>
+                            <p>Keep Trying</p>
+                            <button onClick={closeModal}>Close</button>
+                        </div>
+                    )}
                 </div>
             )}
-            {!isCorrect && (
-                <div>
-                    <h1>Never Give Up!!</h1>
-                    <p className='solution'>Answer: {sol}</p>
-                    <p>Keep Trying</p>
-                    {/* <button onClick={onClose}>Close</button> */}
-                </div>
-            )}
-        </div>
+        </>
     )
 }
